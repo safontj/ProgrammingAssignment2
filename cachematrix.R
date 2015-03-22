365543b39faa3a7fdae94bf110017a2f6b792ccb
Jordi Safont <girwa@hotmail.com>
2015-03-22 16:16:29 +0100

    cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,39 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## makeCacheMatrix creates a list to operate with a matrix and the cache
+
+## cacheSolve makes use of the previous functions to retrieve the inverse
+## matrix from the cache, or calculate ir and load it into cache if it
+## was not previously loaded
+
+
 ## Write a short comment describing this function
-## initiates the m variable in the cahe and
-## creates a list of functions to work on the matrix
+## makeCacheMatrix initializes the cache (m) and loads a matrix (x)
+## then creates a list of functions to work on the matrix and the cache
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         
+        
+        ## list of functions to use on matrix (x) and cache (m)
+        
+        ## loads a new matrix and clears the cache from previous inverse values
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
+        
+        ## gets the matrix to be inverted
         get <- function() x
+        
+        ## loads the inverse value into the cache (m)
         setinverse <- function(inverse) m <<- inverse
+        
+        ## gets the inverse value from the cache (m)
         getinverse <- function() m
+        
+        
         list(set = set, get = get,
              setinverse = setinverse,
              getinverse = getinverse)
@@ -23,7 +42,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-## if getinverse is not null (m in cahe alredy has a value) gets that value
+## cacheSolve retrieves the inverse form cache (m) if already loaded
 ## otherwise calculates de inverse and stores it in cache (m) 
 
 cacheSolve <- function(x, ...) {
@@ -33,6 +52,8 @@ cacheSolve <- function(x, ...) {
                 return(m)
         }
         data <- x$get()
+        
+        ## only for square matrix with determinant !=0
         m <- solve(data, ...)
         x$setinverse(m)
         m
